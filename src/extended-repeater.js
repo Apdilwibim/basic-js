9f4e47b70b6501449b8929f8ce9281660ba18dd6
Pavel <apdilwibim@gmail.com>
2020-10-05 23:12:04 +0300

    feat: add solution for two task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -3,13 +3,12 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function repeater(str, options) {
   if (options.separator == undefined) options.separator = '+';
   if (options.additionSeparator == undefined) options.additionSeparator = '|';
-
   if (options.repeatTimes == undefined) options.separator = '', options.repeatTimes = 1;
-
   if (options.additionRepeatTimes == undefined) options.additionSeparator = '', options.additionRepeatTimes = 1;
 
   let addition = (`${options.addition}${options.additionSeparator}`).repeat(options.additionRepeatTimes);
   let additions = addition.substr(0, addition.length - options.additionSeparator.length);
+
   if (additions == 'undefined') additions = '';
 
   let result = (`${str}${additions}${options.separator}`).repeat(options.repeatTimes);
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,12 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
+let max = 0;
 module.exports = class DepthCalculator {
   constructor() {
     this.calculateDepth = this.calculateDepth.bind(this);
   }
-  calculateDepth(value) {
-    return Array.isArray(value) ?
-      1 + Math.max(...value.map(this.calculateDepth)) :
-      0;
+  calculateDepth(value, current = 1) {
+    for (let i = 0; i < value.length; i++) {
+      if (Array.isArray(value[i])) max = this.calculateDepth(value[i], current + 1);
+    }
+    max = (max > current) ? max : current;
+    let dep = max;
+    max = 0;
+    return dep;
   }
 };
